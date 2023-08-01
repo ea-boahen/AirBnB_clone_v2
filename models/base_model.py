509a9b4ff6a492fea47f43dd611d88b4cf8c3ce8
ea-boahen <ea.boahen2015@gmail.com>
2023-08-01 03:12:35 +0000

    Airbnb clone v2

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,12 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+           default_datetime = datetime.strptime('2000-01-01T00:00:00.000000',
+                                                 '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs.setdefault('updated_at', default_datetime)
+            kwargs.setdefault('created_at', default_datetime)
+            kwargs.pop('__class__', None)
+
             self.__dict__.update(kwargs)
 
     def __str__(self):

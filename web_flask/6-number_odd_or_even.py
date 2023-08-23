e0c90916db83f0e61a09dc9e99f8f556cba84473
ea-boahen <ea.boahen2015@gmail.com>
2023-08-23 21:05:02 +0000

    web flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -71,11 +71,12 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    """Displays an HTML page only if <n> is an integer.
-
-    States whether <n> is odd or even in the body.
-    """
-    return render_template("6-number_odd_or_even.html", n=n)
+    """Displays an HTML page only if <n> is an integer."""
+    f isinstance(n, int):
+        result = "even" if n % 2 == 0 else "odd"
+        """States whether <n> is odd or even in the body."""
+        return render_template("6-number_odd_or_even.html", n=n, evenness=result)
+    return "Invalid input: n must be an integer"
 
 
 if __name__ == "__main__":

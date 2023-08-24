63078d57b51baa55936b46f97c2e9202b7ac75cb
ea-boahen <ea.boahen2015@gmail.com>
2023-08-24 12:02:12 -0700

    deploy documentation 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,8 @@ from fabric.api import run, put, env
 env.hosts = ['44.192.38.3', '3.239.82.120']
 env.user = "ubuntu"
 
- """
+def do_deploy(archive_path):
+"""
     Deploy a compressed archive to the web servers.
 
     This function uploads the specified archive to the remote servers, extracts
@@ -21,7 +22,7 @@ env.user = "ubuntu"
     Returns:
         bool: True if deployment is successful, False otherwise.
  """
-def do_deploy(archive_path):
+
     if not os.path.exists(archive_path):
         return False
 
@@ -29,19 +30,19 @@ def do_deploy(archive_path):
     archive_name = os.path.splitext(archive_filename)[0]
 
     try:
-	# Upload the archive to the server's /tmp/ directory
+	"""Upload the archive to the server's /tmp/ directory"""
         put(archive_path, '/tmp/')
-        # Create the necessary release directory
+         """ Create the necessary release directory"""
         run('sudo mkdir -p /data/web_static/releases/{}/'.format(archive_name))
-        # Extract the archive contents
+         """ Extract the archive contents"""
         run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_name))
-        # Clean up the uploaded archive
+        """ Clean up the uploaded archive"""
         run('sudo rm /tmp/{}'.format(archive_filename))
-        # Move the contents to the appropriate location
+        """ Move the contents to the appropriate location"""
         run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_name, archive_name))
-        # Clean up the old web_static directory
+        """ Clean up the old web_static directory"""
         run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(archive_name))
-        # Update the symbolic link to point to the new version
+        """ Update the symbolic link to point to the new version"""
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_name))
         print("New version deployed!")

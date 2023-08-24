f66b591dd49d7ce8b4c57ee6caf750b260ba2c8b
ea-boahen <ea.boahen2015@gmail.com>
2023-08-24 11:11:21 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,19 @@ from fabric.api import run, put, env
 env.hosts = ['44.192.38.3', '3.239.82.120']
 env.user = "ubuntu"
 
+ """
+    Deploy a compressed archive to the web servers.
 
+    This function uploads the specified archive to the remote servers, extracts
+    its contents, updates the symbolic link, and deploys the new version of the
+    static website.
+
+    Args:
+        archive_path (str): Path to the archive file to be deployed.
+
+    Returns:
+        bool: True if deployment is successful, False otherwise.
+ """
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
